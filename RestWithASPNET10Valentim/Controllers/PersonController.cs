c8e533f974ea0d0bbadda6de51858cacaae5100d
valentimhelio <valentimhelio@gmail.com>
2025-12-18 16:04:35 -0400

    45. 0602 Implementando as Classes Controller e Model

---
+++ RestWithASPNET10Valentim/Controllers/PersonController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithASPNET10Valentim.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+    }
+}
