e8024c83a44b23acf33853ce0aa64123b7e9aba5
valentimhelio <valentimhelio@gmail.com>
2026-01-07 14:19:51 -0400

    53. 0704 Configurando as Propriedades de Conexão com a Base de Dados no SQL Server

---
+++ RestWithASPNET10Valentim/Configurations/DatabaseConfig.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestWithASPNET10Valentim.Model.Context;
+
+namespace RestWithASPNET10Valentim.Configurations;
+
+public static class DatabaseConfig
+{
+    public static IServiceCollection AddDatabaseConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration["MSSQLServerSQLConnection:MSSQLServerSQLConnectionString"];
+
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            throw new ArgumentNullException("Connection string 'MSSQLServerSQLConnectionString' not found.");
+        }
+
+        services.AddDbContext<MSSQLContext>(options => options.UseSqlServer(connectionString));
+        return services;
+    }
+}
--- RestWithASPNET10Valentim/Program.cs
+++ RestWithASPNET10Valentim/Program.cs
@@ -1,3 +1,4 @@
+using RestWithASPNET10Valentim.Configurations;
 using RestWithASPNET10Valentim.Services;
 using RestWithASPNET10Valentim.Services.Impl;
 using Scalar.AspNetCore;
@@ -7,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDatabaseConfiguration(builder.Configuration);
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -20,8 +24,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    //app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "RestWithAPSNET10Valentim"));
-    app.MapScalarApiReference();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "RestWithAPSNET10Valentim"));
+    //app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
